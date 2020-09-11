b1c665b915edf3e04a175ecce04b19075c4be644
Gonzalo Flores <gonzaflores358@gmail.com>
2020-09-10 23:03:40 -0300

    .

--- README.md
+++
@@ -1,3 +0,0 @@
-# todo-list-react
-Una todo list hecha con React
-https://unsplash.com/photos/RYtiT3b7XW4
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './views/home.js';
-import './style.css';
-
-ReactDOM.render(<Home />, document.getElementById('root'));
